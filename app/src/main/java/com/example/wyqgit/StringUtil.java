f8c5b6affcf4b4fde6e49e77b8a5568480ac5713
小魏 <1765898736@qq.com>
2021-11-10 13:26:31 +0800

    aaa

--- app/src/main/java/com/example/wyqgit/StringUtil.java
+++ app/src/main/java/com/example/wyqgit/StringUtil.java
@@ -7,7 +7,7 @@ package com.example.wyqgit;
  */
 public class StringUtil {
 
-    //aaa
+    //aaakkk
     public boolean isNull(String str){
         if (str.equals("") || str == null){
             return true;
