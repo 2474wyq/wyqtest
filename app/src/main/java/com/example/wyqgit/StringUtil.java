64b88092ccb78e6ae75aed7157ea2f4df52f85a7
小魏 <1765898736@qq.com>
2021-11-10 11:28:25 +0800

    first commit

---
+++ app/src/main/java/com/example/wyqgit/StringUtil.java
@@ -0,0 +1,17 @@
+package com.example.wyqgit;
+
+/**
+ * 作者：魏英琪
+ * 时间：2021/11/10
+ * 用途：
+ */
+public class StringUtil {
+
+    public boolean isNull(String str){
+        if (str.equals("") || str == null){
+            return true;
+        }
+        return false;
+    }
+
+}
